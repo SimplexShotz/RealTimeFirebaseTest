603d7af6bf1c8433c2b9cb25a46e5acc64642660
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-02-04 15:13:51 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,22 @@
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyC82qKSxASHV73ab3MhmfZOw1OUryZwEqI",
-  authDomain: "realtimefirebasetest.firebaseapp.com",
-  databaseURL: "https://realtimefirebasetest.firebaseio.com",
-  projectId: "realtimefirebasetest",
-  storageBucket: "realtimefirebasetest.appspot.com",
-  messagingSenderId: "628446710214"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-var ref = {
-  pts: database.ref("pts");
-};
+// var firebase;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC82qKSxASHV73ab3MhmfZOw1OUryZwEqI",
+    authDomain: "realtimefirebasetest.firebaseapp.com",
+    databaseURL: "https://realtimefirebasetest.firebaseio.com",
+    projectId: "realtimefirebasetest",
+    storageBucket: "realtimefirebasetest.appspot.com",
+    messagingSenderId: "628446710214"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  var ref = {
+    pts: database.ref("pts");
+  };
 }
 
 function draw() {
