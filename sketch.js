b29fa2b975f6e8f9107b46d4e8bd681d84f61d5f
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-02-04 17:05:57 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,7 +59,9 @@ var typing = {
 var kp = [];
 var keyPressed = function() {
   kp[keyCode] = true;
-  console.log(keyCode);
+  if (keyCode === 8) {
+    typing.t = typing.t.split("").splice(0, typing.t.split("").length - 1).join("");
+  }
 };
 var keyReleased = function() {
   kp[keyCode] = false;
@@ -67,14 +69,9 @@ var keyReleased = function() {
 
 function keyTyped() {
   if (typing.x !== -1) {
-    console.log(keyCode);
-    console.log(kp[32]);
     if (keyCode !== 8 && keyCode !== 13) {
       typing.t += key;
-    } else if (keyCode === 8) {
-      var p = typing.t.split("").splice(0, typing.t.split("").length - 1).join("");
-      typing.t = p;
-    } else {
+    } else if (keyCode === 13 && kp[16]) {
       var send = {
         t: typing.t,
         x: typing.x / window.innerWidth,
