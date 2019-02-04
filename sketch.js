a7302c4e306581f1f2213702a268bd9854f305b3
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-02-04 15:06:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC82qKSxASHV73ab3MhmfZOw1OUryZwEqI",
+  authDomain: "realtimefirebasetest.firebaseapp.com",
+  databaseURL: "https://realtimefirebasetest.firebaseio.com",
+  projectId: "realtimefirebasetest",
+  storageBucket: "realtimefirebasetest.appspot.com",
+  messagingSenderId: "628446710214"
+};
+firebase.initializeApp(config);
+
 function setup() {
-  
+  createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-  
+  background(250);
 }
