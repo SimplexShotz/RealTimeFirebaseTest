0bb8573172cbe95fe07271edd8b5f1fa411e7b4f
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-02-04 17:06:53 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -69,7 +69,7 @@ var keyReleased = function() {
 
 function keyTyped() {
   if (typing.x !== -1) {
-    if (keyCode !== 8 && keyCode !== 13) {
+    if (keyCode !== 8 && !(keyCode === 13 && kp[16])) {
       typing.t += key;
     } else if (keyCode === 13 && kp[16]) {
       var send = {
